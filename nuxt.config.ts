0ef0fb34da96a69c2f766cd783459f67360383f4
daniel arcos <danielestebanarcos.pro@gmail.com>
2025-08-27 15:36:43 -0500

    feat:optimize system

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,6 +23,9 @@ export default defineNuxtConfig({
   css: ["@/assets/css/main.css"],
   vite: {
     plugins: [tailwindcss()],
+    build: {
+      cssCodeSplit: false,
+    },
   },
   colorMode: {
     classSuffix: "",
